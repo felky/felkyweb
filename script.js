df7742bc6450f2008d0c453b08558867a259e1b0
felky <felixg.nti@gmail.com>
2019-03-19 00:28:42 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var hasBlurred = false;
+$(window).blur(function() {
+        document.title = "already bored?";
+        hasBlurred = true;
+    });
+$(window).focus(function() {
+    if(hasBlurred == true){
+        document.title = "welcome back!"
+    } else {
+        document.title = "felix gramell";
+    }
+});
